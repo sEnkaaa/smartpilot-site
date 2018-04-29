2dd5a4c691142e7d0b320003fe609aac39d86c44
sEnkaaa <senkai6259@gmail.com>
2018-04-29 17:41:26 +0200

    google adsense

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -36,6 +36,17 @@
 		  gtag('config', 'UA-118348111-1');
 		</script>
 
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- landingpage -->
+		<ins class="adsbygoogle"
+		     style="display:block"
+		     data-ad-client="ca-pub-4272783057467322"
+		     data-ad-slot="5960794653"
+		     data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+
 	</head>
 	<body>
 		<!-- WRAPPER -->
